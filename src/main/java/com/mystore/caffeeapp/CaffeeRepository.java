fcabdbf5b5c39bc222ac847066eedade9c8d8495
rotha01 <rothakhon10@gmail.com>
2026-05-29 16:51:08 +0700

    updated constructor injection

--- src/main/java/com/mystore/caffeeapp/CaffeeRepository.java
+++ src/main/java/com/mystore/caffeeapp/CaffeeRepository.java
@@ -12,24 +12,24 @@ import java.util.Optional;
 public class CaffeeRepository {
 
 
-    private final CaffeeConfig caffeeConfig;
-    public CaffeeRepository(CaffeeConfig caffeeConfig) {
+    private final List<Caffee> caffeeConfig;
+    public CaffeeRepository(List<Caffee> caffeeConfig) {
         this.caffeeConfig = caffeeConfig;
     }
 
 
-    private final ArrayList<Caffee> caffeeList = new ArrayList<>(){{
-        add(new CaffeeConfig().caffee1());
-        add(new CaffeeConfig().caffee2());
-        add(new CaffeeConfig().caffee3());
-    }};
+//    private final ArrayList<Caffee> caffeeList = new ArrayList<>(){{
+//        add(new CaffeeConfig().caffee1());
+//        add(new CaffeeConfig().caffee2());
+//        add(new CaffeeConfig().caffee3());
+//    }};
 
     public List<Caffee> getCaffeeList() {
-        return caffeeList;
+        return caffeeConfig;
     }
 
     public Optional<Caffee> getCaffeeByCode(String code) {
-            Optional<Caffee> caffee = caffeeList.stream()
+            Optional<Caffee> caffee = caffeeConfig.stream()
                     .filter(s->s.getCode().equals(code) )
                     .findFirst();
             return caffee;
